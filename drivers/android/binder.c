e7802cd1a49a1f1d4d407281450007ff1f3921c9
Andrew Bresticker <abrestic@chromium.org>
2015-10-23 15:13:42 -0700

    CHROMIUM: android: binder: Fix potential scheduling-while-atomic
    Commit f1e7f0a724f6 ("android: binder: Disable preemption while holding
    the global binder lock.") re-enabled preemption around most of the sites
    where calls to potentially sleeping functions were made, but missed
    __alloc_fd(), which can sleep if the fdtable needs to be resized.
    Re-enable preemption around __alloc_fd() as well as __fd_install() which
    can now sleep in upstream kernels as of commit 8a81252b774b ("fs/file.c:
    don't acquire files->file_lock in fd_install()").
    
    BUG=chrome-os-partner:44012
    TEST=Build and boot on Smaug.
    
    Change-Id: I9819c4b95876f697e75b1b84810b6c520d9c33ec
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/308582
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-by: Riley Andrews <riandrews@google.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -434,7 +434,9 @@ static int task_get_unused_fd_flags(struct binder_proc *proc, int flags)
 	rlim_cur = task_rlimit(proc->tsk, RLIMIT_NOFILE);
 	unlock_task_sighand(proc->tsk, &irqs);
 
+	preempt_enable_no_resched();
 	ret = __alloc_fd(proc->files, 0, rlim_cur, flags);
+	preempt_disable();
 err:
 	mutex_unlock(&proc->files_lock);
 	return ret;
@@ -447,8 +449,11 @@ static void task_fd_install(
 	struct binder_proc *proc, unsigned int fd, struct file *file)
 {
 	mutex_lock(&proc->files_lock);
-	if (proc->files)
+	if (proc->files) {
+		preempt_enable_no_resched();
 		__fd_install(proc->files, fd, file);
+		preempt_disable();
+	}
 	mutex_unlock(&proc->files_lock);
 }
 

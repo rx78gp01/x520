3caf8f999cc4471c25b3dc72299ec379edc3119c
google <android@google.com>
2017-12-06 10:51:12 -0800

    qcacld-2.0: Check on IE length to avoid buffer over-read
    An incorrect IE length can overflow the remaining length variable
    and make IE parsing logic perform a buffer over-read.
    Check on IE length to avoid buffer over-read.
    
    Bug: 63868629
    Change-Id: I20ef6a0136c7a5b602ad15a2fb725f20807b81d0
    CRs-Fixed: 2033195
    Signed-off-by: Ecco Park <eccopark@google.com>

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -14622,7 +14622,14 @@ int wlan_hdd_cfg80211_set_ie( hdd_adapter_t *pAdapter,
         elementId = *genie++;
         eLen  = *genie++;
         remLen -= 2;
-
+        
+        /* Sanity check on eLen */
+        if (eLen > remLen) {
+            hddLog(VOS_TRACE_LEVEL_FATAL, "%s: Invalid IE length[%d] for IE[0x%X]",
+                    __func__, eLen, elementId);
+            VOS_ASSERT(0);
+            return -EINVAL;
+        }
         hddLog(VOS_TRACE_LEVEL_INFO, "%s: IE[0x%X], LEN[%d]",
             __func__, elementId, eLen);
 

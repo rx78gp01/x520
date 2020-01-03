2828c85437cf99d5f9cbe7e8f9b7d5f1cc9c8fed
sheenam monga <shebala@codeaurora.org>
2020-01-03 15:20:57 +0530

    wlan: Remove FW memory dump feature.
    FW memory dump feature is no longer used. Hence remove FW memory
    dump feature code changes.
    
    Change-Id: I4800e8e1a047275a83673cc3ee5bf7ed152833d8
    CRs-Fixed: 2592288

--- drivers/staging/prima/CORE/HDD/inc/wlan_hdd_main.h
+++ drivers/staging/prima/CORE/HDD/inc/wlan_hdd_main.h
@@ -1348,8 +1348,6 @@ struct hdd_fw_mem_dump_req_ctx {
  */
 typedef void (*hdd_fw_mem_dump_req_cb)(void *context);
 
-int memdump_init(void);
-int memdump_deinit(void);
 void wlan_hdd_fw_mem_dump_cb(void *,tAniFwrDumpRsp *);
 int wlan_hdd_fw_mem_dump_req(hdd_context_t * pHddCtx);
 void wlan_hdd_fw_mem_dump_req_cb(void *context);
@@ -2061,5 +2059,36 @@ void wlan_hdd_start_sap(hdd_adapter_t *ap_adapter);
  * Return: 0 for success non-zero for failure
  */
 int hdd_drv_cmd_validate(tANI_U8 *command, int len);
+
+/*
+ * hdd_parse_disable_chn_cmd() - Parse the channel list received
+ * in command.
+ * @adapter: pointer to hdd adapter
+ *
+ * @return: 0 on success, Error code on failure
+ */
+int hdd_parse_disable_chan_cmd(hdd_adapter_t *adapter, tANI_U8 *ptr);
+
+/*
+ * hdd_parse_disable_chn_cmd() - get disable channel list
+ * in command.
+ * @hdd_ctx: hdd context
+ * @buf: buffer to hold disable channel list
+ * @buf_len: buffer length
+ *
+ * @return: length of data copied to buf
+ */
+int hdd_get_disable_ch_list(hdd_context_t *hdd_ctx, tANI_U8 *buf,
+                            uint32_t buf_len);
+
+
+/**
+ * hdd_is_cli_iface_up() - check if there is any cli iface up
+ * @hdd_ctx: HDD context
+ *
+ * Return: return true if there is any cli iface(STA/P2P_CLI) is up
+ *         else return false
+ */
+bool hdd_is_cli_iface_up(hdd_context_t *hdd_ctx);
 
 #endif    // end #if !defined( WLAN_HDD_MAIN_H )

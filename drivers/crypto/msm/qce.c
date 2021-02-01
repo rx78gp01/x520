59d1e9816844d25602fc19d185aa71e5607ac774
Monika Singh <monising@codeaurora.org>
2021-02-01 17:02:57 +0530

    crypto: Fix possible stack out of bound error
    Adding fix to check the upper limit on the length
    of the destination array while copying elements
    from source address to avoid stack out of bound error.
    
    Change-Id: I39d5768fa97f9d269cfb101a389bb771d13c7538
    Signed-off-by: Monika Singh <monising@codeaurora.org>

--- drivers/crypto/msm/qce.c
+++ drivers/crypto/msm/qce.c
@@ -789,6 +789,11 @@ static int _ce_setup(struct qce_device *pce_dev, struct qce_req *q_req,
 
 	case CIPHER_ALG_3DES:
 		if (q_req->mode !=  QCE_MODE_ECB) {
+			if (ivsize > MAX_IV_LENGTH) {
+				pr_err("%s: error: Invalid length parameter\n",
+					 __func__);
+				return -EINVAL;
+			}
 			_byte_stream_to_net_words(enciv32, q_req->iv, ivsize);
 			writel_relaxed(enciv32[0], pce_dev->iobase +
 						CRYPTO_CNTR0_IV0_REG);
@@ -814,6 +819,11 @@ static int _ce_setup(struct qce_device *pce_dev, struct qce_req *q_req,
 	case CIPHER_ALG_AES:
 	default:
 		if (q_req->mode !=  QCE_MODE_ECB) {
+			if (ivsize > MAX_IV_LENGTH) {
+				pr_err("%s: error: Invalid length parameter\n",
+					 __func__);
+				return -EINVAL;
+			}
 			_byte_stream_to_net_words(enciv32, q_req->iv, ivsize);
 			writel_relaxed(enciv32[0], pce_dev->iobase +
 						CRYPTO_CNTR0_IV0_REG);
